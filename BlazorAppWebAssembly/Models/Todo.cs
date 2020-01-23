490b07fd754e80dfd3e6be032357bf50c89bd433
Mostafa Ahmed <Mostafa.Ahmed@codelab.sa>
2020-01-23 11:26:07 +0200

    Add project files.

---
+++ BlazorAppWebAssembly/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppWebAssembly.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ BlazorAppWebAssembly/Services/TodoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorAppWebAssembly.Models;
+
+namespace BlazorAppWebAssembly.Services
+{
+    public class TodoService
+    {
+        private static int ID { get; set; } = 1;
+        private static List<Todo> Todos { get; set; }
+        public static List<Todo> GetTodos()
+        {
+            return Todos;
+        }
+        public void AddTodo(Todo todo)
+        {
+            todo.Id = ID;
+            Todos.Add(todo);
+            ID += 1;
+        }
+        public void UpdateTodo(Todo todo)
+        {
+            Todos[todo.Id] = todo;
+        }
+    }
+}
